624f4a232ee395be13ff50beebafd53633eb170e
David Larrimore <davidlarrimore@gmail.com>
2015-07-17 00:05:22 -0400

    updated page title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     mysqli_report(MYSQLI_REPORT_STRICT);
 
     $mysqlError = false;
-    
+
     try {
          $db = new mysqli($hostname, $username, $password, $database);
 
@@ -61,7 +61,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>MixFin</title>
+    <title>David Larrimore CFPB Demo Site</title>
 
       <!-- Latest compiled and minified CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
